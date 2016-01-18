f4e4e29dd99dc9777ff84523ea361c4bf3eafc67
Aaron Happe <ahappe@brownshoe.com>
2016-01-17 19:01:57 -0600

    figuring out cookies

--- app.js
+++ app.js
@@ -31,12 +31,17 @@
         appVue.curPlayer.push(player1);
         var player1Ref = vars.userProfilesRef.child('player1').set(
           player1);
+          // if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player1').set(appVue.curPlayer[0]);
+          	$.cookie("player", 1);
+          // }
         $('.game-play').show();
       },
       initPlayers: function() {
-      $('.game-play').show();
-      $('.not-dasher').hide();
-        var player = {
+	      $('.game-play').show();
+	      $('.not-dasher').hide();
+      	var curPlayerCount = 1;
+        player = {
           playerNum: 0,
           username: this.subUsernames,
           dasher: false,
@@ -48,51 +53,79 @@
             player2.playerNum = this.i;
             this.curPlayer.push(player2);
             vars.userProfilesRef.child('player2').set(player2);
-           
+          if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player2').set(appVue.curPlayer[0]);
+          	curPlayerCount = 2;
+          	$.cookie("player", 2);
+          }
             break;
           case 3:
             var player3 = player;
             player3.playerNum = this.i;
             this.curPlayer.push(player3);
             vars.userProfilesRef.child('player3').set(player3);
-           
+          if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player3').set(appVue.curPlayer[0]);
+          	curPlayerCount = 3;
+          	$.cookie("player", 3);
+          }           
             break;
           case 4:
             var player4 = player;
             player4.playerNum = this.i;
             this.curPlayer.push(player4);
             vars.userProfilesRef.child('player4').set(player4);
-           
+          if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player4').set(appVue.curPlayer[0]);
+          	curPlayerCount = 4;
+          }    
             break;
           case 5:
             var player5 = player;
             player5.playerNum = this.i;
             this.curPlayer.push(player5);
             vars.userProfilesRef.child('player5').set(player5);
-           
+          if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player5').set(appVue.curPlayer[0]);
+          	curPlayerCount = 5;
+          }          
             break;
           case 6:
             var player6 = player;
             player6.playerNum = this.i;
             this.curPlayer.push(player6);
             vars.userProfilesRef.child('player6').set(player6);
-           
+          if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player6').set(appVue.curPlayer[0]);
+          	curPlayerCount = 6;
+          }          
             break;
           case 7:
             var player7 = player;
             player7.playerNum = this.i;
             this.curPlayer.push(player7);
             vars.userProfilesRef.child('player7').set(player7);
-           
+          if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player7').set(appVue.curPlayer[0]);
+          	curPlayerCount = 7;
+          }           
             break;
           case 8:
             var player8 = player;
             player8.playerNum = this.i;
             this.curPlayer.push(player8);
             vars.userProfilesRef.child('player8').set(player8);
-           
+          if (!appVue.allPlayersEntered) {
+          	vars.curPlayerRef.child('player8').set(appVue.curPlayer[0]);
+          	curPlayerCount = 8;
+          }          
             break;
         }
+	        if(appVue.numPlayers == curPlayerCount) {
+        		vars.allPlayersEnteredRef.set({allEntered: true});
+        		funcs.mainGamePlay();
+        		appVue.allPlayersEntered = true;
+	        }
       },
       mainGamePlay: function() {
 
@@ -104,11 +137,38 @@
 	var ref = new Firebase("https://balderdash2.firebaseio.com/");
 	var vars = {
     userProfilesRef: ref.child('usersProfiles'),
-    numPlayersRef: ref.child('numPlayersRef'),
+    numPlayersRef: ref.child('numPlayers'),
     playerCountsRef: ref.child('playerCounts'),
+    curPlayerRef: ref.child("curPlayer"),
+    allPlayersEnteredRef: ref.child("allPlayersEntered"),
+
 	};
 	var funcs = {
 		init: function(){
+			vars.allPlayersEnteredRef.on("value", function(snapshot){
+				var existsSnapA = snapshot.exists(),
+				snapValA = snapshot.val();
+				vars.curPlayerRef.on("value", function(snapshot){
+					if (existsSnapA) {
+						if (snapValA.allEntered == true && !appVue.curPlayer.length) {
+							snapshot.forEach(function(childSnapshot){
+								var childSnapVal = childSnapshot.val(),
+								cookieVal = $.cookie("player");
+								console.log(childSnapVal.playerNum);
+								console.log(cookieVal);
+								if (cookieVal == childSnapshot.playerNum) {
+									appVue.curPlayer.push(childSnapVal);
+									console.log(childSnapVal);
+								}
+
+								if ("3" == 3) {
+									console.log("omg");
+								}
+							});
+						}
+					}
+				});
+			});
       vars.numPlayersRef.on("value", function(snapshot) {
 				funcs.dasherEntered(snapshot);
       });
@@ -119,7 +179,7 @@
       funcs.onUserProfilesRefChildAdded();
 		},
 		dasherEntered: function(snapshot){
-      existsSnap = snapshot.exists();
+      var existsSnap = snapshot.exists();
       if (existsSnap) {
         var valA = snapshot.val();
         appVue.numPlayers = valA.numberPlayers;
@@ -143,7 +203,7 @@
 	    	}
 	    });
 		},
-		initPlayersVue: function(snapshot) {
+		initPlayersVue: function(snapshot){
       var playersEntered = snapshot.val();
       if (appVue.playersEntered.length < appVue.numPlayers){
         appVue.playersEntered.push(playersEntered.playerNum);   
@@ -156,10 +216,12 @@
           	if (!appVue.allPlayersEntered) {
               appVue.players.push(snapshot.val());
           	}
-          })
+          });
         });
-        appVue.allPlayersEntered = true;
       }
+		},
+		mainGamePlay: function(){
+
 		},
 
 	};
